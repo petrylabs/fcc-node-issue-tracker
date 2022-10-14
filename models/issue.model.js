16da6c71c362368fda87dcbfa5bb77982ed35a19
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-10-14 11:03:58 -0300

    Create issue model

---
+++ models/issue.model.js
@@ -0,0 +1,47 @@
+const Schema = require('mongoose').Schema;
+const SchemaObjectId = Schema.Types.ObjectId;
+
+const IssueSchema = new Schema({
+    // Required
+    issue_title: {
+        type: String,
+        required: true
+    },
+    issue_text: {
+        type: String,
+        required: true
+    },
+    created_by: {
+        type: String,
+        required: true
+    },
+    // Optional
+    assigned_to: {
+        type: String,
+        required: false
+    },
+    status_text: {
+        type: String,
+        required: false
+    },
+    // System
+    created_on: {
+        type: String,
+        required: true
+    },
+    open: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+IssueSchema.methods.logThis = function() {
+    console.log('This is a reference to the instance', this);
+}
+
+IssueSchema.statics.logModel = function() {
+    console.log('this is a reference to the model', this);
+}
+
+module.exports = IssueSchema;
